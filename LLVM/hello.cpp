a515b9e0f2e16ffb7b3cc16f916d25dcefa33afb
gentleGuardian123 <takigawa_yoshino@163.com>
2024-05-11 21:23:31 +0800

    added a test main in LLVM/hello.cpp (little modified in run.sh)

--- LLVM/hello.cpp
+++ LLVM/hello.cpp
@@ -1,5 +1,9 @@
 #include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
+#include <llvm/IR/Instructions.h>
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
@@ -30,3 +34,31 @@ static RegisterStandardPasses Y(PassManagerBuilder::EP_EarlyAsPossible,
   [](const PassManagerBuilder &Builder, legacy::PassManagerBase &PM) {
     PM.add(new Hello());
   });
+
+int main() {
+  std::string ir_filename = "../test.ll";
+
+  LLVMContext context;
+  SMDiagnostic error;
+
+  // Parse the LLVM IR file into a Module
+  std::unique_ptr<Module> M = parseIRFile(ir_filename, error, context);
+
+  if (!M) {
+    // If parsing failed, print error message
+    error.print("hello", errs());
+    return 1;
+  }
+
+  // Create an instance of the Hello pass
+  Hello hello_pass;
+  Module *Mod = M.get();
+
+  // Iterate over functions in the module
+    for (Function &F: *Mod) {
+    // Run the Hello pass on each function
+    hello_pass.runOnFunction(F);
+  }
+
+  return 0;
+}
--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 # 确定 LLVM 的版本和工具的位置
 LLVM_CONFIG="llvm-config"
 
+CLANG_COMPILER="clang++"
+
 # 检查 llvm-config 工具是否可用
 if ! [ -x "$(command -v $LLVM_CONFIG)" ]; then
   echo "llvm-config not found. Please ensure LLVM is correctly installed."
@@ -14,14 +16,14 @@ CXXFLAGS=$($LLVM_CONFIG --cxxflags)
 LDFLAGS=$($LLVM_CONFIG --ldflags)
 
 # 编译 llvm_log.cpp 生成共享库 llvm_log.so
-clang++-12 $CXXFLAGS -Wl,-znodelete -fno-rtti -fPIC -shared ./LLVM/llvm_log.cpp -o ./LLVM/llvm_log.so $LDFLAGS
-# clang++-12 $CXXFLAGS -Wl,-znodelete -fno-rtti -fPIC -shared ./LLVM/hello.cpp -o ./LLVM/hello.so $LDFLAGS
+# $CLANG_COMPILER $CXXFLAGS -Wl,-znodelete -fno-rtti -fPIC -shared ./LLVM/llvm_log.cpp -o ./LLVM/llvm_log.so $LDFLAGS
+$CLANG_COMPILER $CXXFLAGS -Wl,-znodelete -fno-rtti -fPIC -shared ./LLVM/hello.cpp -o ./LLVM/hello.so $LDFLAGS
 
 # 使用 llvm_log.so 处理 test.cpp
-clang++-12 -Xclang -load -Xclang ./LLVM/llvm_log.so -c test.cpp -o ./tmp/test.o
-# clang++-12 -Xclang -load -Xclang ./LLVM/hello.so -c test.cpp -o ./tmp/test_hello.o
+# $CLANG_COMPILER -Xclang -load -Xclang ./LLVM/llvm_log.so -c test.cpp -o ./tmp/test.o
+$CLANG_COMPILER -Xclang -load -Xclang ./LLVM/hello.so -c test.cpp -o ./tmp/test_hello.o
 
 # 编译 logFunc.cpp 生成链接文件 logFunc.o
-clang++-12 -c ./LLVM/logFunc.cpp -o ./tmp/logFunc.o
+$CLANG_COMPILER -c ./LLVM/logFunc.cpp -o ./tmp/logFunc.o
 
-clang++-12 ./tmp/test.o ./tmp/logFunc.o -o test
+$CLANG_COMPILER ./tmp/test_hello.o ./tmp/logFunc.o -o test_hello
